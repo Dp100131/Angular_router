8753406baad65a8296f7dc431f1426556a206e1c
Dp100131 <apintod@uninorte.edu.co>
2023-09-21 22:50:41 -0500

    router link and active

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service'
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-nav',
@@ -14,16 +16,19 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [];
 
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
+    private categoriesService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -34,6 +39,13 @@ export class NavComponent implements OnInit {
     this.authService.loginAndGet('john@mail.com', 'changeme')
     .subscribe(user => {
       this.profile = user;
+    });
+  }
+
+  getAllCategories(){
+    this.categoriesService.getAll()
+    .subscribe(data => {
+      this.categories = data;
     });
   }
 
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 import { ProductsService } from 'src/app/services/products.service';
-
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  template: '<app-products (LoadMore)="onLoadMore()" [products]="products"></app-products>',
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit{
@@ -19,14 +19,18 @@ export class CategoryComponent implements OnInit{
   constructor(private route: ActivatedRoute, private productsService: ProductsService){}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id')
-      if(this.categoryId){
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset).subscribe(data => {
-          this.products = data;
-        })
-      }
-
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id')
+        if(this.categoryId){
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+        return []
+      })
+    )
+    .subscribe(data => {
+      this.products = data;
     })
   }
 
